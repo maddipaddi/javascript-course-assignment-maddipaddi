204bc62308e0861d406dd4ea835eb0e6c2ac39d4
maddipaddi <madelen.sletteberg@gmail.com>
2024-02-23 09:19:25 +0100

    Had to change set up to resolve errors, and drop routing system :'(

---
+++ js/pages/index.mjs
@@ -0,0 +1,54 @@
+
+import { API_SQUARE_EYES } from "../common/constants.mjs";
+import { doFetch } from "../utils/doFetch.mjs";
+
+
+async function init() {
+    try {
+      const response = await doFetch(API_SQUARE_EYES);
+      const movies = response.data;
+      displayMovies(movies);
+      console.log(movies);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+init();
+
+
+// Movie list on homepage functions
+function createMovieListContent(movie){
+    
+    const movieTitle = document.createElement("h3");
+    movieTitle.textContent = movie.title; 
+
+    const movieImg = document.createElement("img");
+    movieImg.setAttribute("src", movie.image.url);
+    movieImg.classList.add("movie-list-images");
+
+    const moviePrice = document.createElement("p");
+    moviePrice.textContent = `Price: ${movie.price} NOK`; 
+
+    const seeMoreButton = document.createElement("a"); 
+    seeMoreButton.setAttribute("href", `/product.html?id=${movie.id}`);
+    seeMoreButton.classList.add("cta-button");
+    seeMoreButton.classList.add("lesser-cta");
+    seeMoreButton.textContent = "See more";
+
+    const movieProduct = document.createElement("div");
+    movieProduct.append(movieTitle, movieImg, moviePrice, seeMoreButton);
+    return movieProduct;
+}
+
+function displayMovies(movies){
+    const movieListContainer = document.getElementById("movie-list-container");
+    console.log(movieListContainer);
+    movies.forEach(movie => {
+       
+       movieListContainer.appendChild(createMovieListContent(movie)); 
+    });
+
+    
+}
+
--- js/pages/product.mjs
+++ js/pages/product.mjs
@@ -1,58 +1,16 @@
+import { API_SQUARE_EYES } from "../common/constants.mjs";
+import { doFetch } from "../utils/doFetch.mjs";
 
-import { API_SQUARE_EYES } from "./common/constants.mjs";
-import { doFetch } from "./utils/doFetch.mjs";
-import { router } from "./utils/routing.mjs";
 
-async function main () {
+async function init() {
     try {
-      await router();
-      const response = await doFetch(API_SQUARE_EYES);
-      const movies = response.data;
-      displayMovies(movies);
-      console.log(movies);
+      displayMovieDetails();
     } catch (error) {
         console.log(error);
     }
 }
 
-main();
-
-
-// Movie list on homepage functions
-function createMovieListContent(movie){
-    
-    const movieTitle = document.createElement("h3");
-    movieTitle.textContent = movie.title; 
-
-    const movieImg = document.createElement("img");
-    movieImg.setAttribute("src", movie.image.url);
-    movieImg.classList.add("movie-list-images");
-
-    const moviePrice = document.createElement("p");
-    moviePrice.textContent = `Price: ${movie.price} NOK`; 
-
-    const seeMoreButton = document.createElement("a"); 
-    seeMoreButton.setAttribute("href", `/product.html?id=${movie.id}`);
-    seeMoreButton.classList.add("cta-button");
-    seeMoreButton.classList.add("lesser-cta");
-    seeMoreButton.textContent = "See more";
-
-    const movieProduct = document.createElement("div");
-    movieProduct.append(movieTitle, movieImg, moviePrice, seeMoreButton);
-    return movieProduct;
-}
-
-function displayMovies(movies){
-    const movieListContainer = document.getElementById("movie-list-container");
-    console.log(movieListContainer);
-    movies.forEach(movie => {
-       
-       movieListContainer.appendChild(createMovieListContent(movie)); 
-    });
-
-    
-}
-
+init();
 
 // Product page details functions 
 function getMovieId() {
@@ -61,10 +19,10 @@ function getMovieId() {
     return movieId;
 } 
 
-export async function renderMovieDetails() {
-    const movieId = getMovieId();
+    async function renderMovieDetails() {
     const response = await doFetch(API_SQUARE_EYES);
     const movies = response.data;
+    const movieId = getMovieId();
     const movie = movies.find(movie => movie.id === movieId);
     
     const movieTitle = document.createElement("h1");
@@ -97,10 +55,9 @@ export async function renderMovieDetails() {
     return movieDetails;
 } 
 
-export async function displayMovieDetails(){
+    async function displayMovieDetails(){
     const displayMovieContainer = document.getElementById("display-movie-container");
     const movieDetails = await renderMovieDetails();
     displayMovieContainer.append(movieDetails);
-
 }
 
